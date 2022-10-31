dbe385ffea518bf0774865476f812ceb87419edf
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-10-31 23:11:01 +0530

    cLEAN uP

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,6 @@
 from flask import Flask,render_template,request,url_for,redirect
-import string
-import random
 app = Flask(__name__)
-GlobalHash='2e'
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -17,15 +15,13 @@ def login():
 
         for i in range(len(Usernames)):
             if(Username == Usernames[i] and Password==Passwords[i]): 
-                Hash= ''.join(random.choices(string.ascii_lowercase +string.digits, k=10))
-                GlobalHash=Hash
-                return(redirect(url_for("username",Hash=Hash)))
+                return(redirect(url_for("Dash",Username=Username)))
         else:
           return render_template("login.html")
     return render_template('login.html')
 
 
-@app.route('/<Hash>')
-def username(Hash):
-    return render_template('admin.html')
+@app.route('/Admin=<Username>')
+def Dash(Username):
+    return render_template('Dashboard.html',Username=Username)
     
