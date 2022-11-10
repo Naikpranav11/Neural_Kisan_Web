1a4a51ac0239b12901e8f3ad4a78d108fcf9683e
RhysJosmin <55994011+Rhys-011003@users.noreply.github.com>
2022-11-10 11:47:14 +0530

    Ml model

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,13 @@
-from flask import Flask,render_template,request,url_for,redirect,Response
+from flask import Flask,render_template,request,url_for,redirect,Response,stream_with_context
 import cv2
 import math
+import json
+import random
+import time
+import numpy as np
+from keras.models import load_model
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -39,10 +46,53 @@ def gen_frames():
         else:
             ret, buffer = cv2.imencode('.jpg', (frame))
             frame = buffer.tobytes()
+            
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/Result')
+def res():
+    return Response(classify(camera,'Classification Model\keras_model.h5','Classification Model\labels.txt'),mimetype='text/plain')
 
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+data=[]
+
+@app.route('/chart_data')
+def chart_data():
+    
+    def generate_random_data():  
+        while True:
+            data.append(random.random()) 
+            yield f'{random.random()},\n'
+            time.sleep(1)
+           
+
+    return Response(generate_random_data(),mimetype='text/plain')
+
+
+def classify(img,modelFile,labels):
+     # Load the model
+        model = load_model(modelFile)
+
+        camera = img
+
+        labels = open(labels, 'r').readlines()
+        while True:
+
+        
+
+            ret, image = camera.read()
+            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+            image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+            image = (image / 127.5) - 1
+            probabilities = model.predict(image)
+
+            yield f'{labels[np.argmax(probabilities)]}'
+
+
+
+
