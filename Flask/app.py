6a11891cfeb8d8d5e7f95f0b7231bb7774f48670
RhysJosmin <55994011+Rhys-011003@users.noreply.github.com>
2022-11-17 19:43:25 +0530

    POst API

--- Flask/app.py
+++ Flask/app.py
@@ -5,7 +5,7 @@ import json
 import random
 import time
 import numpy as np
-from keras.models import load_model
+#from keras.models import load_model
 from datetime import datetime
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ def index():
 @app.route('/Login' , methods=["POST","GET"])
 def login():
     Usernames=['Rhys','Sai','Pranav','Kapil','Ralph',]
-    Passwords=['123','123','123','123']
+    Passwords=['123','123','123','123','123']
     if request.method == "POST":   
         Username=request.form['Username']
         Password=request.form['Password']
@@ -34,8 +34,8 @@ def login():
 def Dash(Username):
     return render_template('Dashboard.html',Username=Username)
     
-
-camera = cv2.VideoCapture(0)
+CamSrc=0
+camera = cv2.VideoCapture(CamSrc)
 
 
 
@@ -52,10 +52,39 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/Result')
-def res():
-    return classify(camera,'Classification Model\keras_model.h5','Classification Model\labels.txt')
+#@app.route('/Result')
+#def res():
+#    return classify(camera,'Classification Model\keras_model.h5','Classification Model\labels.txt')
 
+@app.route('/ResultG')
+def result():
+    WaterLevel=round(random.random(),2)
+    LightLevel=round(random.random(),2)
+    if WaterLevel>0.5:
+        WateringResult='Add Water'
+    else:
+        WateringResult='Dont Water'
+
+
+    r=round(random.random()*255,2)
+    g=round(random.random()*255,2)
+    b=round(random.random()*255,2)
+    a=round(random.random(),2)
+    lightRGB=f'rgba({r},{g},{b},{a})'
+
+    temperature=round(random.random()*30,2)
+
+    if temperature>20:
+        TempCommand='Cool'
+    elif temperature<10:
+        TempCommand='Warm'
+    else:
+        TempCommand='Normal'
+    DiseaseList=['none','Aster Yellows','Bacterial Wilt','Blight','Canker','Crown Gall','Rot','Scab']
+    cameraSource=CamSrc
+    Disease=DiseaseList[random.randint(0,6)]
+
+    return json.dumps({'Water':{'Sensor Water Level':f'{WaterLevel}','Watering Result':f'{WateringResult}'},'Light':{'Sensor Light Level':f'{LightLevel}','RGBA':f'{lightRGB}'},'Cooling':{'Temperature':f'{temperature}','Command':f'{TempCommand}'},'Camera':{'src':f'{cameraSource}','Disease':f'{Disease}'}})
 
 @app.route('/video_feed')
 def video_feed():
@@ -63,37 +92,28 @@ def video_feed():
 
 data=[]
 
-@app.route('/Random')
-def chart_data():
-    
-    def generate_random_data():  
-        
-        data.append(random.random()) 
-        return json.dumps({'VALUE':random.random()})
-           
-
-    return generate_random_data()
 
 
-def classify(img,modelFile,labels):
+
+#def classify(img,modelFile,labels):
      # Load the model
-        model = load_model(modelFile)
-
-        camera = img
-
-        labels = open(labels, 'r').readlines()
-        
-
-        
-
-        ret, image = camera.read()
-        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-        image = (image / 127.5) - 1
-        probabilities = model.predict(image)
-        ret ={'LABEL':f'{labels[np.argmax(probabilities)]}','MAX':f'{np.max(probabilities)}','MIN':f'{np.min(probabilities)}'}
-        json_dump=json.dumps(ret)
-        return json_dump
+#        model = load_model(modelFile)
+#
+#        camera = img
+#
+#        labels = open(labels, 'r').readlines()
+#        
+#
+#        
+#
+#        ret, image = camera.read()
+#        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+#        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+#        image = (image / 127.5) - 1
+#        probabilities = model.predict(image)
+#        ret ={'LABEL':f'{labels[np.argmax(probabilities)]}','MAX':f'{np.max(probabilities)}','MIN':f'{np.min(probabilities)}'}
+#        json_dump=json.dumps(ret)
+#        return json_dump
 
 
 
---
+++ Flask/static/javascript/Data Processing.js
@@ -0,0 +1,15 @@
+
+setInterval(f,5000)
+
+
+const light=Document.g
+    function f(){
+        fetch('http://127.0.0.1:5000/ResultG')
+        .then(res=>res.json())
+        .then(data=>{
+           
+            console.log(data)
+           
+          
+        })
+    }
