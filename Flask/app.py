468806d7d49ff32fba318d44a29b7e161ef42eb3
Sai Rane <sairane2828@gmail.com>
2022-11-30 17:40:37 +0530

    rhyso is nuts

--- Flask/app.py
+++ Flask/app.py
@@ -5,6 +5,12 @@ import json
 import random
 import time
 import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+import joblib
+
 #from keras.models import load_model
 from datetime import datetime
 
@@ -52,9 +58,9 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-#@app.route('/Result')
-#def res():
-#    return classify(camera,'Classification Model\keras_model.h5','Classification Model\labels.txt')
+@app.route('/Result')
+def res():
+   return classify(camera,'Classification Model\keras_model.h5','Classification Model\labels.txt')
 
 @app.route('/ResultG')
 def result():
@@ -93,33 +99,81 @@ def video_feed():
 data=[]
 
 
+
+class DM:
+    def __init__():
+        return 0
+    def Train(self,csvPath):
+        df = pd.read_csv(csvPath)
+
+        iris = datasets.load_iris()
+
+        import pandas as pd
+        data=pd.DataFrame({
+        'sepal length':iris.data[:,0],
+        'sepal width':iris.data[:,1],
+        'petal length':iris.data[:,2],
+        'petal width':iris.data[:,3],
+        'species':iris.target
+            })
+        data.head()
+
+
+        X=data[['sepal length', 'sepal width', 'petal length', 'petal width']]  # Features
+        y=data['species']  # Labels
+
+        # Split dataset into training set and test set
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) # 70% training and 30% test
+        from sklearn.ensemble import RandomForestClassifier
+        
+        #Create a Gaussian Classifier
+        clf=RandomForestClassifier(n_estimators=100)
+
+        #Train the model using the training sets y_pred=clf.predict(X_test)
+        clf.fit(X_train,y_train)
+
+        
+        print('Trained')
+        # joblib.dump(clf,'model.pkl')
+        # print("Model Saved")
+    def Classify(self):
+        # lr=joblib.load('model.pkl')
+        y_pred= self.clf.predict(self.X_test)
+        species_idx = self.clf.predict([[3, 5, 4, 2]])[0]
+        return self.iris.target_names[species_idx]
+
+
 ##PRANAV ENTER CODE HERE
 @app.route('/Decision')
-def Decision_maker():
+def Decision_maker(csvPath):
     
-    return json.dumps()
+    dum=DM()
+    dum.Train('iris.csv')
+    
+
+    return dum.Classify()
 
 ##PRANAV ENTER CODE HERE
 
-#def classify(img,modelFile,labels):
-     # Load the model
-#        model = load_model(modelFile)
-#
-#        camera = img
-#
-#        labels = open(labels, 'r').readlines()
-#        
-#
-#        
-#
-#        ret, image = camera.read()
-#        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-#        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-#        image = (image / 127.5) - 1
-#        probabilities = model.predict(image)
-#        ret ={'LABEL':f'{labels[np.argmax(probabilities)]}','MAX':f'{np.max(probabilities)}','MIN':f'{np.min(probabilities)}'}
-#        json_dump=json.dumps(ret)
-#        return json_dump
+def classify(img,modelFile,labels):
+     ##Load the model
+       model = load_model(modelFile)
+
+       camera = img
+
+       labels = open(labels, 'r').readlines()
+       
+
+       
+
+       ret, image = camera.read()
+       image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+       image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+       image = (image / 127.5) - 1
+       probabilities = model.predict(image)
+       ret ={'LABEL':f'{labels[np.argmax(probabilities)]}','MAX':f'{np.max(probabilities)}','MIN':f'{np.min(probabilities)}'}
+       json_dump=json.dumps(ret)
+       return json_dump
 
 
 
